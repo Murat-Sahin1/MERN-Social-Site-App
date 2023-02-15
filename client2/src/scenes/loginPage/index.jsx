934d80617030e4d5aae4afe26dc02b110406c9ff
Murat Şahin <18155057@mersin.edu.tr>
2023-02-16 02:12:37 +0300

    LoginPage layout setup.

--- client2/src/scenes/loginPage/index.jsx
+++ client2/src/scenes/loginPage/index.jsx
@@ -1,5 +1,49 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 
 const LoginPage = () => {
-    return (<div>loginPASDGASDGASDGage</div>)
-}
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const theme = useTheme();
+  const alt = theme.palette.background.alt;
+  const background = theme.palette.background.default;
+  return (
+    <Box>
+      <Box
+        width="100%"
+        padding="1% 6%"
+        display={"flex"}
+        justifyContent="center"
+        textAlign={"center"}
+        backgroundColor={alt}
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Socialsite
+        </Typography>
+      </Box>
+
+      <Box
+        display={"flex"}
+        justifyContent="center"
+        backgroundColor={background}
+      >
+        <Box
+          justifyContent={"center"}
+          backgroundColor={alt}
+          width={isNonMobileScreens ? "50%" : "100%"}
+          height="49rem"
+          margin={isNonMobileScreens ? "2rem" : "2rem 4rem"}
+          marginLeft={isNonMobileScreens ? "6rem" : "4rem"}
+          
+          borderRadius={"1.5rem"}
+        ></Box>
+        {isNonMobileScreens && (<Box
+          backgroundColor={"red"}
+          borderRadius={"1.5rem"}
+          height={isNonMobileScreens ? "30rem" : ""}
+          width={isNonMobileScreens ? "20%" : ""}
+          margin={isNonMobileScreens ? "2rem" : ""}
+        ></Box>)}
+      </Box>
+    </Box>
+  );
+};
 export default LoginPage;
