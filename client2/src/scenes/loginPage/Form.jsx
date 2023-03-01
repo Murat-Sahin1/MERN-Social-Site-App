a63a3ebeb514ea99bca253caa07874a5af47ec1f
Murat Şahin <18155057@mersin.edu.tr>
2023-03-01 11:03:52 +0300

    Some changes are made on LoginPage

---
+++ client2/src/scenes/loginPage/Form.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import {
+    Box,
+    Button,
+    TextField,
+    useMediaQuery,
+    Typography,
+    useTheme
+} from "@mui/material";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import { Formik } from "formik";
+import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setLogin } from "state";
+import Dropzone from "react-dropzone";
+import FlexBetween from "components/FlexBetween";
+
+
--- client2/src/scenes/loginPage/index.jsx
+++ client2/src/scenes/loginPage/index.jsx
@@ -1,10 +1,15 @@
 import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 
 const LoginPage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   const theme = useTheme();
   const alt = theme.palette.background.alt;
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
   const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const primaryMain = theme.palette.primary.main;
   return (
     <Box>
       <Box
@@ -24,24 +29,37 @@ const LoginPage = () => {
         display={"flex"}
         justifyContent="center"
         backgroundColor={background}
+        height="100%"
+        borderColor={primaryLight}
+        border="Highlight"
       >
         <Box
           justifyContent={"center"}
           backgroundColor={alt}
           width={isNonMobileScreens ? "50%" : "100%"}
           height="49rem"
-          margin={isNonMobileScreens ? "2rem" : "2rem 4rem"}
+          p="1rem 6%"
+          marginTop={isNonMobileScreens ? "2rem" : "2rem"}
           marginLeft={isNonMobileScreens ? "6rem" : "4rem"}
-          
+          mb="2rem"
+          mr={isNonMobileScreens ? "" : "4rem"}
+          textAlign="center"
           borderRadius={"1.5rem"}
-        ></Box>
-        {isNonMobileScreens && (<Box
-          backgroundColor={"red"}
-          borderRadius={"1.5rem"}
-          height={isNonMobileScreens ? "30rem" : ""}
-          width={isNonMobileScreens ? "20%" : ""}
-          margin={isNonMobileScreens ? "2rem" : ""}
-        ></Box>)}
+        >
+          <Typography fontWeight={"500"} variant="h5" sx={{ mb: "1.rem" }}>
+            Welcome to Socialsite, the new kind of social media.
+          </Typography>
+          <Form />
+        </Box>
+        {isNonMobileScreens && (
+          <Box
+            backgroundColor={"red"}
+            borderRadius={"1.5rem"}
+            height={isNonMobileScreens ? "30rem" : ""}
+            width={isNonMobileScreens ? "20%" : ""}
+            margin={isNonMobileScreens ? "2rem" : ""}
+          ></Box>
+        )}
       </Box>
     </Box>
   );
