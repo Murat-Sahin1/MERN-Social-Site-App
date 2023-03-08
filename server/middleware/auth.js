e59f6d1e424590e4f2b6459531a03bd9e67ba84f
Murat Şahin <18155057@mersin.edu.tr>
2023-03-09 02:06:11 +0300

    JWT, verifyToken is fixed.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,7 +8,7 @@ export const verifyToken = async(req, res, next) => {
         }
 
         if(token.startsWith("Bearer ")){
-            token = token.slice(7, tokens.length).trimLeft();
+            token = token.slice(7, token.length).trimLeft();
         }
 
         const verified = jwt.verify(token, process.env.JWT_SECRET);
